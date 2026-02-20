fb159d2b0fc8604db8a16afd21dafef14017faf7
Blackbi <ongbivlog3107@gmail.com>
2026-02-20 21:40:39 +0700

    Change the config

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import torch
 # Setting hyperparams
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LR = 2e-4
-NOISE_DIM = 100
-IMG_DIM = 128 * 256
-BATCH_SIZE = 32
-EPOCHS = 50
+BATCH_SIZE = 1
+EPOCHS = 100
 
 LAMBDA_CYC = 10
 # I'm not sure with this lambda
 LAMBDA_ID = 5
+
+SAVE_MODEL = True
+LOAD_MODEL = False
